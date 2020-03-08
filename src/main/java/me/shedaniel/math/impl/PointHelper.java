b94b80bb73a627bc3a33dceb7e88aa912053faa2
shedaniel <daniel@shedaniel.me>
2020-03-08 18:23:11 +0800

    2.11.1
    Signed-off-by: shedaniel <daniel@shedaniel.me>

--- src/main/java/me/shedaniel/math/impl/PointHelper.java
+++ src/main/java/me/shedaniel/math/impl/PointHelper.java
@@ -1,17 +1,21 @@
 package me.shedaniel.math.impl;
 
-import me.shedaniel.math.api.Point;
+import me.shedaniel.math.Point;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 
 @Environment(EnvType.CLIENT)
 public class PointHelper {
-    public static Point fromMouse() {
+    public static me.shedaniel.math.api.Point fromMouse() {
         MinecraftClient client = MinecraftClient.getInstance();
         double mx = client.mouse.getX() * (double) client.getWindow().getScaledWidth() / (double) client.getWindow().getWidth();
         double my = client.mouse.getY() * (double) client.getWindow().getScaledHeight() / (double) client.getWindow().getHeight();
-        return new Point(mx, my);
+        return new me.shedaniel.math.api.Point(mx, my);
+    }
+    
+    public static Point ofMouse() {
+        return fromMouse();
     }
     
     public static int getMouseX() {
