2fe295019f229e8d80607d429bdc5631f167b857
shedaniel <daniel@shedaniel.me>
2020-12-31 20:15:39 +0800

    Mark mod clientside only for networking

--- src/main/java/me/shedaniel/clothconfig2/forge/ClothConfig.java
+++ src/main/java/me/shedaniel/clothconfig2/forge/ClothConfig.java
@@ -2,11 +2,16 @@ package me.shedaniel.clothconfig2.forge;
 
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod("cloth-config")
 public class ClothConfig {
     public ClothConfig() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ClothConfigInitializer::registerModsPage);
     }
 }
