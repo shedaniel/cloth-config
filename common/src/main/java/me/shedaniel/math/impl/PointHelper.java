ad272c015e07517f1f69f2c0f65e3bfb29f0b5dd
shedaniel <daniel@shedaniel.me>
2021-11-10 18:16:29 +0800

    Update basic math to 0.6.0

--- common/src/main/java/me/shedaniel/math/impl/PointHelper.java
+++ common/src/main/java/me/shedaniel/math/impl/PointHelper.java
@@ -19,6 +19,7 @@
 
 package me.shedaniel.math.impl;
 
+import me.shedaniel.math.FloatingPoint;
 import me.shedaniel.math.Point;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -33,11 +34,26 @@ public class PointHelper {
         return new Point(mx, my);
     }
     
+    public static FloatingPoint ofFloatingMouse() {
+        Minecraft client = Minecraft.getInstance();
+        double mx = client.mouseHandler.xpos() * (double) client.getWindow().getGuiScaledWidth() / (double) client.getWindow().getScreenWidth();
+        double my = client.mouseHandler.ypos() * (double) client.getWindow().getGuiScaledHeight() / (double) client.getWindow().getScreenHeight();
+        return new FloatingPoint(mx, my);
+    }
+    
     public static int getMouseX() {
         return ofMouse().x;
     }
     
     public static int getMouseY() {
         return ofMouse().y;
+    }
+    
+    public static double getMouseFloatingX() {
+        return ofFloatingMouse().x;
+    }
+    
+    public static double getMouseFloatingY() {
+        return ofFloatingMouse().y;
     }
 }
