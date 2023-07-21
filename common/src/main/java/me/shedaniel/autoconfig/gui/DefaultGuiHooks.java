d789d6ca5f5c3fdff4f9526448ca1d18cc16150f
Matt Sturgeon <matt@sturgeon.me.uk>
2023-07-21 21:37:49 +0100

    Add debug prints

--- common/src/main/java/me/shedaniel/autoconfig/gui/DefaultGuiHooks.java
+++ common/src/main/java/me/shedaniel/autoconfig/gui/DefaultGuiHooks.java
@@ -20,14 +20,37 @@
 package me.shedaniel.autoconfig.gui;
 
 import me.shedaniel.autoconfig.gui.registry.GuiRegistry;
+import me.shedaniel.clothconfig2.api.AbstractConfigListEntry;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Environment(EnvType.CLIENT)
 public class DefaultGuiHooks {
     private DefaultGuiHooks() {}
     
     public static GuiRegistry apply(GuiRegistry registry) {
+        
+        // FIXME Remove debugging code
+        registry.registerPreHook((guis, i18n, field, config, defaults, guiProvider) ->
+                System.out.println(String.join("\n",
+                        "Pre hook for `%s#%s`:".formatted(field.getDeclaringClass().getSimpleName(), field.getName()),
+                        " - \"%s\"".formatted(i18n)
+                )));
+        
+        registry.registerPostHook((guis, i18n, field, config, defaults, guiProvider) ->
+                System.out.println(Stream.concat(
+                        Stream.of(
+                                "Post hook for `%s#%s`:".formatted(field.getDeclaringClass().getSimpleName(), field.getName()),
+                                " - \"%s\"".formatted(i18n)
+                        ),
+                        guis.stream().map(Object::getClass).map(Class::getSimpleName).map(name -> " - " + name)
+                ).collect(Collectors.joining("\n"))));
+        
         return registry;
     }
 }
