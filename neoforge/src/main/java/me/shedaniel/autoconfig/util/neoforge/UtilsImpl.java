cf91e9bc35ffba26b5fec7a62185fd16d7f60a93
shedaniel <daniel@shedaniel.me>
2023-11-19 01:12:29 +0800

    Fix neoforge crash

--- neoforge/src/main/java/me/shedaniel/autoconfig/util/neoforge/UtilsImpl.java
+++ neoforge/src/main/java/me/shedaniel/autoconfig/util/neoforge/UtilsImpl.java
@@ -17,7 +17,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-package me.shedaniel.autoconfig.util.forge;
+package me.shedaniel.autoconfig.util.neoforge;
 
 import net.neoforged.fml.loading.FMLPaths;
 
